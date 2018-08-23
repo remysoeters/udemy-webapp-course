1a462d8ebf039b41b04e0029548a3fc9c2131d9c
Remy Soeters <RSoeters@arpa.local>
2018-08-23 13:52:25 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next)=>{
+    res.render('maintainance.hbs', {
+        pageTitle: 'Maintainance',
+        pageMessage:'This page is currently under construction, we will be back shortly'
+    })
+})
+
+app.use((req, res, next)=>{
+
+    var now = new Date().toString()
+    var log = `${now} ${req.method} ${req.url}`
+    // log opslaan in server.log
+    fs.appendFile('server.log', log + '\n', (err)=>{
+        if(err){
+            console.log('Unable to append to server.log')
+        }
+    })
+
+
+    next()    
+})
+
+
+
+
+
+
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase()
+})
+
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello World</h1>')
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage:'Welkom op deze pagina, je vindt hier verschillende dingen.'
+    })
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    })
+})
+
+app.get('/bad', (req, res) =>{
+    res.send({
+        errorMessage:'Unable to handle request'
+    })
+})
+
+
+
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port:3000')
+})
